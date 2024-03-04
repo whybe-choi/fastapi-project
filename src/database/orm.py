7fb527b6ca7af3b6b48efcb48cd6331d517c1af7
whybe-choi <choi980912@khu.ac.kr>
2024-03-04 18:15:02 +0900

    fix: Correct __repr__

--- src/database/orm.py
+++ src/database/orm.py
@@ -3,13 +3,13 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-class Todo(Base):
-    __tablename__ = "todos"
+class ToDo(Base):
+    __tablename__ = "todo"
 
     id = Column(Integer, primary_key=True, index=True)
-    content = Column(String(256), nullable=False)
+    contents = Column(String(256), nullable=False)
     is_done = Column(Boolean, nullable=False)
 
     # 객체를 출력할 때 보기 쉬운 형태로 보기 위해서 repr이라는 magic method를 override
     def __repr__(self):
-        return f"Todo(id={self.id}, content={self.content}, is_done={self.is_done})"
+        return f"Todo(id={self.id}, contents={self.contents}, is_done={self.is_done})"
