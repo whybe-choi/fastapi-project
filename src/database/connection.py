bbd61e1ea1e1257fc66adc21fee018fae664dbe6
whybe-choi <choi980912@khu.ac.kr>
2024-03-04 18:11:34 +0900

    feat: Check database connection

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import select
+from orm import ToDo
 
 DATABASE_URL = "mysql+pymysql://root:todos@127.0.0.1:3306/todos"
 
@@ -8,4 +9,4 @@ engine = create_engine(DATABASE_URL, echo=True)
 SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 session = SessionFactory()
-session.scalar(select(1))
+print(list(session.scalars(select(ToDo))))
