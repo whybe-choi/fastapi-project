fc94951db7da9d07484e4719095a59d53addb649
whybe-choi <choi980912@khu.ac.kr>
2024-03-05 23:43:46 +0900

    refactor: Update get_todos_handler

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI, Body, HTTPException
+from typing import List
+from fastapi import FastAPI, Body, HTTPException, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from database.orm import ToDo
+from database.repository import get_todos
+from database.connection import get_db
 
 app = FastAPI()
 
@@ -27,11 +32,14 @@ todo_data = {
 
 # 전체 To-Do 조회
 @app.get("/todos", status_code=200)
-def get_todos_handler(order : str | None = None):
-    ret = list(todo_data.values())
+def get_todos_handler(
+    order : str | None = None,
+    session: Session = Depends(get_db),
+):
+    todos: List[ToDo] = get_todos(session=session)
     if order == "DESC":
-        return ret[::-1]
-    return ret
+        return todos[::-1]
+    return todos
 
 # 단일 To-Do 조회
 @app.get("/todos/{todo_id}", status_code=200)
