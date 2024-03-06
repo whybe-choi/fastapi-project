538f448bd3e230a7510025194a652d219a0a9d35
whybe-choi <choi980912@khu.ac.kr>
2024-03-06 18:49:25 +0900

    feat: Create schema.py

---
+++ src/schema/response.py
@@ -0,0 +1,13 @@
+from typing import List
+from pydantic import BaseModel
+
+class ToDoSchema(BaseModel):
+    id: int
+    contents: str
+    is_done: bool
+
+    class Config:
+        orm_mode = True
+
+class ListToDoResponse(BaseModel):
+    todos: List[ToDoSchema]
