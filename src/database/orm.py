f5e6bccf6f75fb43cbbe6392ead62ca8f4888d23
whybe-choi <choi980912@khu.ac.kr>
2024-03-08 18:22:43 +0900

    feat: Define instance method for is_done

--- src/database/orm.py
+++ src/database/orm.py
@@ -1,3 +1,4 @@
+from re import T
 from sqlalchemy import Boolean, Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
@@ -23,3 +24,13 @@ class ToDo(Base):
             contents=request.contents,
             is_done=request.is_done
         )
+    
+    # 유지보수의 편의성을 위해 instance method 정의
+    # 전체적인 시스템의 안정성을 위해 일반적으로 데이터를 변경할 때는 instance method를 이용하는 것이 좋다.
+    def done(self) -> "ToDo":
+        self.is_done = True
+        return self
+
+    def undone(self) -> "ToDo":
+        self.is_done = False
+        return self
