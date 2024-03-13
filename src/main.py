3e84f32844d212f804627536adc0182ce82a853b
whybe-choi <choi980912@khu.ac.kr>
2024-03-13 14:58:39 +0900

    refactor: Use ToDoRepository class

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import FastAPI, Body, HTTPException, Depends
 from sqlalchemy.orm import Session
 from database.orm import ToDo
-from database.repository import create_todo, delete_todo, get_todo_by_todo_id, get_todos, update_todo
+from database.repository import ToDoRepository
 from database.connection import get_db
 from schema.request import CreateToDoRequest
 from schema.response import ToDoListSchema, ToDoSchema
@@ -18,9 +18,9 @@ def health_check_handler():
 @app.get("/todos", status_code=200)
 def get_todos_handler(
     order : str | None = None,
-    session: Session = Depends(get_db),
+    todo_repo: ToDoRepository = Depends(ToDoRepository)
 ) -> ToDoListSchema:
-    todos: List[ToDo] = get_todos(session=session)
+    todos: List[ToDo] = todo_repo.get_todos()
     if order == "DESC":
         return ToDoListSchema(
             todos=[ToDoSchema.from_orm(todo) for todo in todos[::-1]]
@@ -33,9 +33,9 @@ def get_todos_handler(
 @app.get("/todos/{todo_id}", status_code=200)
 def get_todo_handler(
     todo_id : int,
-    session: Session = Depends(get_db)
+    todo_repo: ToDoRepository = Depends(ToDoRepository)
 ) -> ToDoSchema:
-    todo : ToDo | None = get_todo_by_todo_id(session=session, todo_id=todo_id)
+    todo : ToDo | None = todo_repo.get_todo_by_todo_id(todo_id=todo_id)
     if todo:
         return ToDoSchema.from_orm(todo)
     raise HTTPException(status_code=404, detail="ToDo Not Found")
@@ -44,10 +44,10 @@ def get_todo_handler(
 @app.post("/todos", status_code=201)
 def create_todo_handler(
     request: CreateToDoRequest,
-    session: Session = Depends(get_db)
+    todo_repo: ToDoRepository = Depends(ToDoRepository)
 ) -> ToDoSchema:
     todo: ToDo = ToDo.create(request=request)
-    todo: ToDo = create_todo(session=session, todo=todo)
+    todo: ToDo = todo_repo.create_todo(todo=todo)
     return ToDoSchema.from_orm(todo)
 
 # To-Do 수정
@@ -55,22 +55,22 @@ def create_todo_handler(
 def update_todo_handler(
     todo_id : int, 
     is_done: bool = Body(..., embed=True),
-    session: Session = Depends(get_db)
+    todo_repo: ToDoRepository = Depends(ToDoRepository)
 ):
-    todo : ToDo | None = get_todo_by_todo_id(session=session, todo_id=todo_id)
+    todo : ToDo | None = todo_repo.get_todo_by_todo_id(todo_id=todo_id)
     if todo:
         todo.done() if is_done else todo.undone()
-        todo: ToDo | None = update_todo(session=session, todo=todo)
+        todo: ToDo | None = todo_repo.update_todo(todo=todo)
         return ToDoSchema.from_orm(todo)
     raise HTTPException(status_code=404, detail="ToDo Not Found")
 
 @app.delete("/todos/{todo_id}", status_code=204)
 def delete_todo_handler(
     todo_id : int,
-    session: Session = Depends(get_db),
+    todo_repo: ToDoRepository = Depends(ToDoRepository)
 ):
-    todo : ToDo | None = get_todo_by_todo_id(session=session, todo_id=todo_id)
+    todo : ToDo | None = todo_repo.get_todo_by_todo_id(todo_id=todo_id)
     if not todo:
         raise HTTPException(status_code=404, detail="ToDo Not Found")
     
-    delete_todo(session=session, todo_id=todo_id)
+    todo_repo.delete_todo(todo_id=todo_id)
