37aea1d442015bafe95e60aadf857bf05cc01753
whybe-choi <choi980912@khu.ac.kr>
2024-03-18 23:27:49 +0900

    feat: Add user router

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from api import todo
+from api import todo, user
 
 app = FastAPI()
 app.include_router(todo.router)
+app.include_router(user.router)
 
 @app.get("/")
 def health_check_handler():
