d2f5efd4d4d3ee4c90cc28819c5a59b625a26fad
whybe-choi <choi980912@khu.ac.kr>
2024-03-20 22:39:01 +0900

    feat: Create user_sign_up_handler

--- src/api/user.py
+++ src/api/user.py
@@ -1,8 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from schema.request import SignUpRequest
+from schema.response import UserSchema
+from database.orm import User
+from database.repository import UserRepository
+from service.user import UserService
 
 
 router = APIRouter(prefix="/users")
 
-@router.post("/sign-up")
-def user_sign_up_handler():
-    return True
+@router.post("/sign-up", status_code=201)
+def user_sign_up_handler(
+    request: SignUpRequest,
+    user_service: UserService = Depends(),
+    user_repo: UserRepository = Depends()
+):
+    # 1. request body(username, password)
+    # 2. password -> hashing -> hashed_password
+    hashed_password = user_service.hash_password(
+        plain_password=request.password
+    )
+    # 3. User(username, hashed_password)
+    user: User = User.create(
+        username=request.username, hashed_password=hashed_password
+    )
+    # 4. user -> db save
+    user: User = user_repo.save_user(user=user) # id : int
+    # 5. return user(id, username)
+    return UserSchema.from_orm(user)
