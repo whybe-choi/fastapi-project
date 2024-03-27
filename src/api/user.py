c95cfa25f1aacca260b830e2cb5c363c9ba5b512
whybe-choi <choi980912@khu.ac.kr>
2024-03-27 23:42:29 +0900

    feat: Define user_log_in_handler

--- src/api/user.py
+++ src/api/user.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends
-from schema.request import SignUpRequest
+from fastapi import APIRouter, Depends, HTTPException
+from schema.request import LoginRequest, SignUpRequest
 from schema.response import UserSchema
 from database.orm import User
 from database.repository import UserRepository
@@ -29,10 +29,29 @@ def user_sign_up_handler(
     return UserSchema.from_orm(user)
 
 @router.post("/log-in")
-def user_log_in_handler():
+def user_log_in_handler(
+    request: LoginRequest,
+    user_service: UserService = Depends(),
+    user_repo: UserRepository = Depends()
+):
     # 1. request body(username, password)
     # 2. db read user
+    user: User | None =  user_repo.get_user_by_username(
+        username=request.username
+    )
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
     # 3. user.password, request.password -> bcrypt.checkpw
+    verified: bool = user_service.verify_password(
+        plain_password=request.password, hashed_password=user.hashed_password
+    )
+
+    if not verified:
+        raise HTTPException(status_code=401, detail="Not Authorized")
+    
     # 4. create jwt
+    jwt = user_service.create_jwt(username=user.username)
+
     # 5. return jwt
-    return True
+    return jwt
